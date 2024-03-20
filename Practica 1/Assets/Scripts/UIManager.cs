801d34714c57fd74ddb47c4bbd4935b24f853a61
samuel <samuga05@ucm.es>
2024-03-20 12:50:12 +0100

    comentarios

--- Practica 1/Assets/Scripts/UIManager.cs
+++ Practica 1/Assets/Scripts/UIManager.cs
@@ -15,18 +15,26 @@ public class UIManager : MonoBehaviour
 
         UQueryBuilder<VisualElement> builder = new(rootve);
 
+        //elemento del texto de seleccion de campeon
         VisualElement contenedor = builder.Name("SelectText");
+
+        //todos los elemtos con la clase "textFont"
         VisualElement contenedor2 = builder.Class("textFont");
 
+        //listas de elementos
         List<VisualElement> visualElements = contenedor.Children().ToList();
         List<VisualElement> visualElements2 = builder.Class("textFont").ToList();
 
+
+        //el texto de seleccion de campeon pasa a tener la clase "textFont"
         visualElements.ForEach(elem =>
         {
             Debug.Log(elem.name);
             elem.AddToClassList("textFont");
         });
 
+
+        //todos los elementos pierden la clase "textFont" y se les anade "textFontRed"
         visualElements2.ForEach(elem =>
         {
             Debug.Log(elem.name);
