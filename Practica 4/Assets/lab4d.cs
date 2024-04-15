c33621894ca2749b0a59a85921408fe1a549448b
samuel <samuga05@ucm.es>
2024-04-15 12:52:51 +0200

    inicio lab 4

---
+++ Practica 4/Assets/lab4d.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class lab4d : VisualElement
+{
+    public new class UxmlFactory : UxmlFactory<lab4d> { }
+
+    public lab4d() { 
+    
+        
+    }
+}
