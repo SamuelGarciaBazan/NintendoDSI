b8c9c322466eac83b81cd73d514ca4954313afac
samuel <samuga05@ucm.es>
2024-04-16 16:39:46 +0200

    avances lab 4

--- Practica 4/Assets/lab4d.cs
+++ Practica 4/Assets/lab4d.cs
@@ -1,14 +1,81 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Numerics;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UIElements;
 
 public class lab4d : VisualElement
 {
-    public new class UxmlFactory : UxmlFactory<lab4d> { }
+    public new class UxmlFactory : UxmlFactory<lab4d,UxmlTraits> { }
 
-    public lab4d() { 
+    public new class UxmlTraits : VisualElement.UxmlTraits
+    {
+
+        UxmlIntAttributeDescription numb = new UxmlIntAttributeDescription { name=  "Items", defaultValue = 0 };
+
+        public override void Init(VisualElement ve, IUxmlAttributes bag, CreationContext cc)
+        {
+            base.Init(ve, bag, cc);
+
+            var semaforo = ve as lab4d;
+            semaforo.Items = numb.GetValueFromBag(bag, cc);
+            Debug.Log("N Items : " + semaforo.Items);
+
+        }
+    }
+
+    public Background style;
+
+    int nItems;
+    public int Items
+    {
+        get =>nItems; 
+        set {
+            nItems = value;
+            prueba();
+        } 
+    }
     
+    List<VisualElement> items = new List<VisualElement>();
+
+    public lab4d() {
+
+        string image = "";
+
+        //style = Resources.Load<StyleSheet>("Images/espada").ConvertTo<StyleBackground>();
         
+        VisualElement item;
+        for (int i = 0; i < 5; i++)
+        {
+            item  = new VisualElement();
+
+            item.style.width = 100;
+            item.style.height = 100;
+            item.name = "item" + i.ToString();  
+            item.style.backgroundColor = Color.white;   
+            //item.style.backgroundImage = style;   
+            items.Add(item);
+
+            hierarchy.Add(item);
+        }
+
+        
+    }
+
+    void prueba()
+    {
+
+        VisualElement item;
+
+        for(int i = 0; i < 5; i++)
+        {
+            items[i].style.backgroundColor = Color.white;
+        }
+
+        for (int i = 0; i < Mathf.Min(nItems,5) ; i++)
+        {
+            items[i].style.backgroundColor = Color.green;
+        }
     }
 }
